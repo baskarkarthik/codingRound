9396f2a311f6fbfede0718a8d113fb5d9ac1d505
KARTHIK <karthikravi128@gmail.com>
2018-05-02 23:48:38 +0530

    1)Fixed Null pointer exceptions - Initialize all locator variable and functions in class to static, called the function initElements( , ) 2)Textbox and calendar issue(unable to select value from drop down) -Fixed textbox and calendar issues

--- src/main/java/HotelBookingTest.java
+++ src/main/java/HotelBookingTest.java
@@ -5,32 +5,57 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+
+import java.util.List;
+import org.openqa.selenium.support.How;
+ 
+import org.openqa.selenium.support.PageFactory;
 
 public class HotelBookingTest {
 
-    WebDriver driver = null;
+	static WebDriver driver = null;
 
     @FindBy(linkText = "Hotels")
-    private WebElement hotelLink;
+    static WebElement hotelLink;
 
     @FindBy(id = "Tags")
-    private WebElement localityTextBox;
+    static WebElement localityTextBox;
 
     @FindBy(id = "SearchHotelsButton")
-    private WebElement searchButton;
+    static WebElement searchButton;
 
     @FindBy(id = "travellersOnhome")
-    private WebElement travellerSelection;
-
+    static WebElement travellerSelection;
+    
+   
+   
     @Test
-    public void shouldBeAbleToSearchForHotels() {
+    public static void shouldBeAbleToSearchForHotels() {
+    	
+    	
         setDriverPath();
         driver = new ChromeDriver();
         driver.get("https://www.cleartrip.com/");
+        
+        PageFactory.initElements(driver, HotelBookingTest.class);
+        
         hotelLink.click();
 
         localityTextBox.sendKeys("Indiranagar, Bangalore");
+        waitFor(5000);
+        driver.findElement(By.xpath("//a[text()='Indiranagar, Bangalore, Karnataka, India']")).click();
+       // List<WebElement> originOptions = driver.findElement(By.id("ui-id-1")).findElements(By.tagName("li"));
+        //originOptions.get(0).click();
 
+        waitFor(2000);
+        
+        driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr[3]/td[6]/a")).click();
+        waitFor(2000);
+        
+        driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr[3]/td[7]/a")).click();
+
+        
         new Select(travellerSelection).selectByVisibleText("1 room, 2 adults");
         searchButton.click();
 
@@ -38,7 +63,7 @@ public class HotelBookingTest {
 
     }
 
-    private void setDriverPath() {
+    public static void setDriverPath() {
         if (PlatformUtil.isMac()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver");
         }
@@ -47,6 +72,14 @@ public class HotelBookingTest {
         }
         if (PlatformUtil.isLinux()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
+        }
+    }
+    
+    public static void waitFor(int durationInMilliSeconds) {
+        try {
+            Thread.sleep(durationInMilliSeconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
     }
 
